720ec8d89f2d4ad721b7150216f486c02a6e55ed
Rangdra22 <63014773+Rangdra22@users.noreply.github.com>
2020-10-25 03:55:48 +0700

    deploy heroku

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 
 // Connect to MongoDB via Mongoose
 seeder.connect(
-  "mongodb://localhost:27017/db_staycation",
+  "mongodb+srv://rangdra:rangdra123@cluster0.tyrce.mongodb.net/db_staycation?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
