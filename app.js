7a4a5a28dc4b639b80cd92fb0052a3ff7eb1d73b
rangdra <rangdra2@gmail.com>
2021-12-26 18:34:30 +0700

    ganti mongo url

--- app.js
+++ app.js
@@ -1,58 +1,60 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-const methodOverride = require("method-override");
-const session = require("express-session");
-const flash = require("connect-flash");
+var createError = require('http-errors');
+const dotenv = require('dotenv');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 //import mongoose
-const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://rangdra:rangdra123@cluster0.tyrce.mongodb.net/db_staycation?retryWrites=true&w=majority",
-  {
+
+dotenv.config();
+const mongoose = require('mongoose');
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  }
-);
+  })
+  .then(() => console.log('MongoDB Connected'));
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 //router admin
-const adminRouter = require("./routes/admin");
-const apiRouter = require("./routes/api");
+const adminRouter = require('./routes/admin');
+const apiRouter = require('./routes/api');
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(methodOverride("_method"));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
 app.use(
   session({
-    secret: "keyboard cat",
+    secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
     cookie: { maxAge: 60000 },
   })
 );
 app.use(flash());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  "/sb-admin-2",
-  express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
+  '/sb-admin-2',
+  express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2'))
 );
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 //admin
-app.use("/admin", adminRouter);
-app.use("/api/v1/member", apiRouter);
+app.use('/admin', adminRouter);
+app.use('/api/v1/member', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -63,11 +65,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
